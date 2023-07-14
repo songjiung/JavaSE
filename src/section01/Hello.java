932c508c482cc9b01e636ab884ad9c334fe9c944
song0 <song0@LAPTOP-ENCBDC7N>
2023-07-14 20:37:59 +0900

    First Commit

---
+++ src/section01/Hello.java
@@ -0,0 +1,12 @@
+package section01;
+/*
+ * 주석
+ * 코드에 영향을 주지 않는 개발자가 사용하는 메모공간
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+		System.out.println("안녕하세요!");
+	}
+
+}
